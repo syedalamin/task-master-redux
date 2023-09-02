45d4687cfd5718f80a2a2c2f2137d6af5d9fcb52
syedalamin <syedalamin2580@gmail.com>
2023-09-02 14:12:36 +0600

    add task modal

--- src/components/tasks/AddTaskModal.jsx
+++ src/components/tasks/AddTaskModal.jsx
@@ -1,9 +1,10 @@
+import Modal from "../ui/Modal";
 
-const AddTaskModal = () => {
+const AddTaskModal = ({isOpen, setIsOpen}) => {
     return (
-        <div>
-            
-        </div>
+        <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero" >
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, eum est id ipsa autem fugiat eius aut nam fuga necessitatibus temporibus cumque nesciunt odit, provident magni reiciendis neque reprehenderit voluptatum!</p>
+      </Modal>
     );
 };
 
